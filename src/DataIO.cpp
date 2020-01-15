e8677b095150caec98f0954f6124e373292f833d
sam0127 <smithsp1@gmail.com>
2020-01-14 23:02:14 -0600

    implemented saving and loading data

--- src/DataIO.cpp
+++ src/DataIO.cpp
@@ -2,10 +2,83 @@
 
 using namespace std;
 
-vector<Course*> readCourses(){
-
+void DataIO::test(string s){
+  ofstream ofs;
+  cout << "yes";
+  ofs.open("../data/test.txt");
+  ofs << s;
+  ofs.close();
 }
 
-void writeCourses(vector<Course*> courses){
+vector<Course*> DataIO::readCourses(){
+  vector<string> files = getFiles();
+  vector<Course*> courses;
+  for(int i = 0; i < files.size(); i++){
+    string filename = files.at(i);
+    size_t lastindex = filename.find_last_of(".");
+    string rawname = filename.substr(0, lastindex);
+    Course* newCourse = new Course(rawname);
+    string path = "/Users/samsmith/Desktop/Projects/GradeKeeper/data/"+filename;
+    ifstream ifs(path);
+    string line;
+    if(ifs.is_open()){
+      while(getline(ifs, line)){
+        vector<string> tokens = tokenizer(line);
+        Category* newCat = new Category(tokens.at(0), atof(tokens.at(1).c_str()));
+        for(int i = 2; i < tokens.size(); i++){
+          newCat->addGrade(atof(tokens.at(i).c_str()));
+        }
+        newCourse->addCategory(newCat);
+      }
+    }
+    ifs.close();
+    courses.push_back(newCourse);
+  }
+  return courses;
+}
 
+void DataIO::writeCourses(vector<Course*> courses){
+  for(int i = 0; i < courses.size(); i++){
+    string filename = courses.at(i)->getName() + ".csv";
+    string path = "/Users/samsmith/Desktop/Projects/GradeKeeper/data/" + filename;
+    ofstream ofs(path);
+    cout << filename << endl;
+    if(ofs.is_open()){
+      vector<Category*> cats = courses.at(i)->getCategories();
+      for(int j = 0; j < cats.size(); j++){
+        cout << cats.at(j)->getName() << endl;
+        ofs << cats.at(j)->getName() << ",";
+        ofs << cats.at(j)->getWeight();
+        for(int k = 0; k < cats.at(j)->getGrades().size(); k++){
+          cout << cats.at(j)->getGrades().at(k) << endl;
+          ofs << "," << cats.at(j)->getGrades().at(k);
+        }
+        ofs << "\n";
+      }
+    }
+    ofs.close();
+  }
+}
+
+vector<string> DataIO::getFiles(){
+  DIR* dir;
+  dirent* pdir;
+  vector<string> files;
+  string path = "/Users/samsmith/Desktop/Projects/GradeKeeper/data/";
+  dir = opendir(path.c_str());
+  while(pdir = readdir(dir)){
+    files.push_back(pdir->d_name);
+  }
+  files.erase(files.begin(), files.begin()+3);
+  return files;
+}
+
+vector<string> DataIO::tokenizer(string line){
+  vector<string> tokens;
+  stringstream ss(line);
+  string intermediate;
+  while(getline(ss, intermediate, ',')){
+    tokens.push_back(intermediate);
+  }
+  return tokens;
 }
--- src/DataIO.h
+++ src/DataIO.h
@@ -3,10 +3,16 @@
 #include "Course.h"
 #include "Category.h"
 #include <fstream>
+#include <dirent.h>
+#include <sstream>
 
 class DataIO{
   public:
+    void test(std::string);
     std::vector<Course*> readCourses();
     void writeCourses(std::vector<Course*>);
+  private:
+    std::vector<std::string> getFiles();
+    std::vector<std::string> tokenizer(std::string);
 };
 #endif
